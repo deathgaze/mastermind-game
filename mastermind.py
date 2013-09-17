080cebaaf8dd31eaebd80a16d429ec12c0c8edef
OpenShift guest <ksefchik@gmail.com>
2013-09-17 14:46:31 -0400

    small reversion

--- mastermind.py
+++ mastermind.py
@@ -14,7 +14,16 @@ def print_title():
     r"    | .  . | __ _ ___| |_ ___ _ __| .  . |_ _ __   __| | ""\n"\
     r"    | |\/| |/ _` / __| __/ _ \ '__| |\/| | | '_ \ / _` | ""\n"\
     r"    | |  | | (_| \__ \ ||  __/ |  | |  | | | | | | (_| | ""\n"\
-    r"    \_|  |_/\__,_|___/\__\___|_|  \_|  |_/_|_| |_|\__,_| ""\n")                                                
+    r"    \_|  |_/\__,_|___/\__\___|_|  \_|  |_/_|_| |_|\__,_| ""\n") 
+
+def print_rules():
+    print("Rules: I am thinking of a secret 4 digit number. Guess my number.")
+    print("- If a digit is in the correct position, I'll mark an O.")
+    print("- If one or more of the digits is in my secret, "\
+        "but NOT in the correct position, I'll mark an X.")
+    print("- If a digit is neither in my secret nor in position, I won't "  \
+        "mark anything.")
+    print("- If a digit is alredy marked, I will not mark it down again.")
 
 def format_guess(guess):
     guess = int(guess)
@@ -37,24 +46,16 @@ def prompt_guess(prompt):
         return format_guess(answer)
     else:
         return False
-
-def play_game():
-    answer = ''
-    secret = str(int(random.random() * 10000))
-    guesses = 1
+        
+class MastermindGame:
+    guesses = 0
+    secret = 
     
-    print_title()
-    print("Rules: I am thinking of a secret 4 digit number. Guess my number.")
-    print("- If a digit is in the correct position, I'll mark an O.")
-    print("- If one or more of the digits is in my secret, "\
-        "but NOT in the correct position, I'll mark an X.")
-    print("- If a digit is neither in my secret nor in position, I won't "  \
-        "mark anything.")
-    print("- If a digit is alredy marked, I will not mark it down again.")
-    
-    while True:
+    def play_round(self, guess):
+        if game_over:
+            return 'OOOO'
+        
         res = ''
-        guess = prompt_guess("Guess #" + str(guesses) + ": ")
         if guess:
             hit_str = ''
             miss_str = ''
@@ -72,14 +73,18 @@ def play_game():
                     res += 'X'
                 
             # Properly format the result before printing it back to the user.
-            print("Result:  ", ''.join(sorted(list(res))))
+            res = ''.join(sorted(list(res)))
             guesses += 1
-            if res == 'OOOO':
-                print("You win! My secret was", secret)
-                print("It took you", guesses, "guesses to find my secret.")
-                break
-        else:
-            print("Invalid guess. Guess must be four integers 0-9.")
+        
+        if res == 'OOOO':
+            self.game_over = True
+
+def play_game():
+    print_title()
+    print_rules()
+    
+    while True:
+
     
     answer = input("Would you like to play again? (y to play again) --> ")
     return answer == 'y'
